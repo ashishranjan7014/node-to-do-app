dcd41e19b9c0a6abba80f466e1d5b209d26a8810
ashishranjan7014 <ashishranjan7014@gmail.com>
2021-08-31 17:00:20 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ import Express from 'express';
 const App = Express();
 const port = process.env.PORT || 8080;
 App.get('/', (req, res) => {
-  res.send('hello brother');
+  let listData = [
+    {
+      email: 'ashishranjan7014@gmail.com',
+      name: 'Ashish Ranjan',
+      contact: '1234567890'
+    },
+    {
+      email: 'kumarvicky@gmail.com',
+      name: 'Vicky Kumar',
+      contact: '4561239870'
+    },
+    {
+      email: 'jblankit7014@gmail.com',
+      name: 'Ankit Kumar',
+      contact: '9876543210'
+    }
+  ];
+  res.send(listData);
 });
 App.listen(port, () => {
   console.log(`Server running on port ${port}`);
